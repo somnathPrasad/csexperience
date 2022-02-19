1e089d8d22a5e011bed874e4fe85de2b9fbb890d
Somnath Prasad <somnathprasad559@gmail.com>
2022-02-19 19:39:36 +0530

    appbar mobile ui, footer

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,33 +1,65 @@
 import { NextPage } from "next";
 import Link from "next/link";
-import styles from "./styles/AppBar.module.css"
+import styles from "./styles/AppBar.module.css";
+import Image from "next/image";
+import { useState } from "react";
+
+const menuIcon = (
+  <Image src="/menu-icon.svg" alt="menu icon" width={35} height={30} />
+);
 
 const AppBar: NextPage = () => {
-    return(
-        <>
-            <div className={`flex justify-evenly h-fit fixed sm:top-0 bottom-0 left-0 w-screen py-5 sm:text-2xl ${styles.glassmorphism} font-extrabold`}>
-                <Link href="/">
-                    <a className="hover:text-cyan-400 ">
-                        Home
-                    </a>
-                </Link>
-                <Link href="/blogs">
-                    <a className="hover:text-cyan-400 ">
-                        Blogs
-                    </a>
-                </Link>
-                <Link href="/tutorials">
-                    <a className="hover:text-cyan-400 ">
-                        Tutorials
-                    </a>
-                </Link>
-                <Link href="/portfolio">
-                    <a className="hover:text-cyan-400 ">
-                        Portfolio
-                    </a>
-                </Link>
-            </div>
-        </>
-    )
-}
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <div
+        className={`h-fit fixed top-0 left-0 w-screen sm:py-6 py-3 sm:text-2xl ${styles.glassmorphism} ${styles.border}`}
+      >
+        <div className="sm:flex hidden justify-around">
+          <Link href="/">
+            <a className="hover:text-cyan-400 ">Home</a>
+          </Link>
+          <Link href="/posts/first-post">
+            <a className="hover:text-cyan-400 ">Blogs</a>
+          </Link>
+          <Link href="/tutorials">
+            <a className="hover:text-cyan-400 ">Tutorials</a>
+          </Link>
+          <Link href="/portfolio">
+            <a className="hover:text-cyan-400 ">Portfolio</a>
+          </Link>
+        </div>
+        <div className="sm:hidden flex justify-end">
+          <div
+            className="px-3"
+            onClick={() => setIsOpen((prevValue) => !prevValue)}
+          >
+            {menuIcon}
+          </div>
+        </div>
+      </div>
+      <div
+        className={`top-[3.7rem] right-0 sm:hidden fixed ${styles.glassmorphism} w-[35vh] h-screen  ${styles.borderSidebar} z-10 transition ease-in-out duration-300 ${isOpen ? "translate-x-0" : "translate-x-full"}`}
+      >
+        <div className="text-center">
+          <div className="flex flex-col py-10 text-xl">
+            <Link href="/">
+              <a className="hover:text-cyan-400 pt-5">Home</a>
+            </Link>
+            <Link href="/blogs">
+              <a className="hover:text-cyan-400 pt-5">Blogs</a>
+            </Link>
+            <Link href="/tutorials">
+              <a className="hover:text-cyan-400 pt-5">Tutorials</a>
+            </Link>
+            <Link href="/portfolio">
+              <a className="hover:text-cyan-400 pt-5">Portfolio</a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default AppBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import AppBar from '../components/AppBar'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import AppBar from "../components/AppBar";
+import Layout from "../components/layout";
+import Link from "next/link";
+
 
 const Home: NextPage = () => {
   return (
-    <div className={`${styles.container} bg-black text-white`}>
+    <Layout>
       <Head>
         <title>CS Experience</title>
         <meta name="description" content="CS Experience home page" />
@@ -14,28 +17,16 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <AppBar/>
-        <div className={`sm:text-8xl text-6xl font-extrabold animationPulseColor`}>
+        <div
+          className={`sm:text-8xl text-6xl font-extrabold animationPulseColor`}
+        >
           <p>Blogs</p>
           <p>Tutorials</p>
           <p>Portfolio</p>
         </div>
       </main>
+    </Layout>
+  );
+};
 
-      {/* <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer> */}
-    </div>
-  )
-}
-
-export default Home
+export default Home;
