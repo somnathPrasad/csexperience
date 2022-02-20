d97a01f95f424e63bcb389245979ee8ac337cb87
Somnath Prasad <somnathprasad559@gmail.com>
2022-02-20 18:23:09 +0530

    copyright text footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,65 +3,71 @@ import footerStyles from "./styles/footer.module.css";
 import ExternalLinkIcon from "./svg/ExternalLinkIcon";
 
 const Footer = () => {
-  
   return (
-    <footer className={`${footerStyles.footer} flex justify-evenly`}>
-      <div>
-        <h1 className="text-lg">Portfolio</h1>
-        <ul className={`text-base ${footerStyles.childHover} text-left`}>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/resume">
-              <a>Resume</a>
-            </Link>
-          </li>
-        </ul>
+    <footer className={`${footerStyles.footer} flex flex-col`}>
+      <div className="flex justify-evenly w-full">
+        <div>
+          <h1 className="text-lg">Portfolio</h1>
+          <ul className={`text-base ${footerStyles.childHover} text-left`}>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/resume">
+                <a>Resume</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h1 className="text-lg">Social</h1>
+          <ul
+            className={`flex flex-col text-base ${footerStyles.childHover} text-left`}
+          >
+            <li>
+              <Link href="https://www.linkedin.com/in/somnath-prasad/">
+                <a target="_blank" className="flex">
+                  <span className="pr-2">LinkedIn</span>
+                  <ExternalLinkIcon />
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="https://www.youtube.com/channel/UCL8q-8ujVGhMYCAVM_bJ-9w">
+                <a target="_blank" className="flex">
+                  <span className="pr-2">Youtube</span>
+                  <ExternalLinkIcon />
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="https://twitter.com/somnathprasad17">
+                <a target="_blank" className="flex items-center">
+                  <span className="pr-2">Twitter</span>
+                  <ExternalLinkIcon />
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="https://github.com/somnathPrasad">
+                <a target="_blank" className="flex items-center">
+                  <span className="pr-2">Github</span>
+                  <ExternalLinkIcon />
+                </a>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div>
-        <h1 className="text-lg">Social</h1>
-        <ul className={`flex flex-col text-base ${footerStyles.childHover} text-left`}>
-          <li>
-            <Link href="https://www.linkedin.com/in/somnath-prasad/">
-              <a target="_blank" className="flex">
-                <span className="pr-2">LinkedIn</span>
-                <ExternalLinkIcon/>
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="https://www.youtube.com/channel/UCL8q-8ujVGhMYCAVM_bJ-9w">
-              <a target="_blank" className="flex">
-                <span className="pr-2">Youtube</span>
-                <ExternalLinkIcon/>
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="https://twitter.com/somnathprasad17">
-              <a target="_blank" className="flex items-center">
-                <span className="pr-2">Twitter</span>
-                <ExternalLinkIcon/>
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="https://github.com/somnathPrasad">
-              <a target="_blank" className="flex items-center">
-                <span className="pr-2">Github</span>
-                <ExternalLinkIcon/>
-              </a>
-            </Link>
-          </li>
-        </ul>
+      <div className="w-full text-center pb-0 mb-0 px-10 pt-5">
+        <p>Copyright © {new Date().getFullYear()} Somnath Prasad. All rights reserved.</p>
       </div>
     </footer>
   );
